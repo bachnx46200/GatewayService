2f41ca92ccc4417caf869d89a9274f703d9ec9fc
nguyễn tuấn anh <62133761+nguyentuananh21798@users.noreply.github.com>
2020-12-07 23:08:19 +0700

    update TintucRespository
    11:08 - 07/12/2020

--- src/main/java/com/example/GatewayService/repository/TinTucRespository.java
+++ src/main/java/com/example/GatewayService/repository/TinTucRespository.java
@@ -2,10 +2,21 @@ package com.example.GatewayService.repository;
 
 import com.example.GatewayService.entity.TinTuc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TinTucRespository extends JpaRepository<TinTuc, UUID> {
+    @Query("select tt from TinTuc tt where tt.loaitintuc=?1")
+    List<TinTuc> findByLoaitintuc(Boolean loaitintuc);
+    
+    
+    @Query(value = "select * from tintuc where tieude ILIKE %?1% and loaitintuc=true ",nativeQuery = true )
+    List<TinTuc> findByTieuDeandTrue(String tieude);
+    
+    @Query(value = "select * from tintuc where tieude ILIKE %?1% and loaitintuc=false ",nativeQuery = true )
+    List<TinTuc> findByTieuDeandFalse(String tieude);
 }
